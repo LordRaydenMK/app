83c4b6b4262b39c382af263a7d0c4561be9cfdf8
Stojan Anastasov <stolea@gmail.com>
2025-03-02 20:15:02 +0100

    Add SQLDelght

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,6 @@ plugins {
     id("com.android.application") version "8.8.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    id("app.cash.sqldelight") version "2.0.2" apply false
     id("app.cash.paparazzi") version "1.3.5" apply false
 }
